9c290aa7923993e388b71184f2fcc8c5184d2d52
Khye Ean <lowkhyeean@gmail.com>
2019-06-08 23:13:37 +1000

    implemented with sendgrid

--- test.py
+++ test.py
@@ -15,13 +15,14 @@ if result == "CAM":
     print("CAM")
 elif result == "HD":
     print("HD")
-message = Mail(
-        from_email='john@wick.com',
-        to_emails='lowkhyeean@gmail.com',
-        subject='John Wick 3 is HD!',
-        html_content='<a href="https://www.putlockers.me/stream-4k/john-wick-chapter-3-parabellum-25636/"> Watch Now! </a>')
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-except Exception as e:
-    print(e.message)
+    message = Mail(
+            from_email='john@wick.com',
+            to_emails='lowkhyeean@gmail.com',
+            subject='dev testing!',
+            html_content='<a href="https://www.putlockers.me/stream-4k/john-wick-chapter-3-parabellum-25636/"> Watch Now! </a>')
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print("sent email")
+    except Exception as e:
+        print(e.message)
